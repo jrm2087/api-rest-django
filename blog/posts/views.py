f4cb3a2b3918842999ce9078aaee42ccd32b81f6
José <63377749+jrm2087@users.noreply.github.com>
2021-09-10 09:13:49 -0500

    modelos y migraciones

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.views.generic.base import View
 
 
 class HelloWorld(View):
     def get(self, request):
-        return HttpResponse(content='hola mundo')
+        data = {
+            'name': 'José Rodríguez L.',
+            'years': 33,
+            'codes': ['Python', 'PHP', 'Java', 'vue']
+        }
+        return render(request, 'hello_world.html', context=data)
