cb82d4009c36e20ce761b39317d108c9a2fc03dd
José <63377749+jrm2087@users.noreply.github.com>
2021-09-12 21:59:48 -0500

    Introduccion rest_framework

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -5,4 +5,4 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     order = models.IntegerField()
-    create_at = models.DateTimeField(auto_created=True)
+    create_at = models.DateTimeField(auto_now_add=True)
