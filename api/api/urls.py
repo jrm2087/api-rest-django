5a293e96e53ffca7bbf00c7e0c016296e44f1b8a
José <63377749+jrm2087@users.noreply.github.com>
2021-09-20 16:42:15 -0500

    Configuracion proyecto API

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version='v1',
+        description="Documentación ApiRest",
+        terms_of_service="",
+        contact=openapi.Contact(email="jrm2087@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    # permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # documentacion
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
